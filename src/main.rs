74382c7d98b5213c15a6673290dd7cad8129e5a4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-23 14:08:57 +0200

    feat: reading $HISTFILE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+const HISTFILE: &str = "HISTFILE";
+
+fn main() -> Result<(), env::VarError> {
+    let hist_path = env::var(HISTFILE)?;
+    println!("The current shell's history file can be found at `{}`", hist_path);
+
+    Ok(())
 }
